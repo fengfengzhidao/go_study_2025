eaea2bd4a30d33f39bc39496adba840c3d841600
fengfeng <fengfengzhidao@126.com>
2025-06-08 22:55:23 +0800

    零值问题

--- 10.布尔类型.go
+++ 10.布尔类型.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	t := true
+	f := false
+	fmt.Println(t, f)
+
+	var tt bool
+	fmt.Println(tt)
 }
---
+++ 11.零值问题.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var i1 uint
+	fmt.Println(i, i1)
+	var s byte
+	fmt.Println(s)
+	var ss string
+	fmt.Printf("%q\n", ss)
+	var b bool
+	fmt.Println(b)
+}
